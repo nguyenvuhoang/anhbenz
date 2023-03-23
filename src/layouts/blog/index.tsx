e69a170884c6f1756a4ec5be03cf3ee801aca66e
nguyenvuhoang <102274002+vknighthub@users.noreply.github.com>
2023-03-23 23:59:27 +0700

    Commit Search Blog

--- src/layouts/blog/index.tsx
+++ src/layouts/blog/index.tsx
@@ -1,9 +1,7 @@
 import client from '@/data/client'
-import {  NewsQueryArrray } from '@/types'
+import { NewsQueryArrray } from '@/types'
 import { useQuery } from '@tanstack/react-query'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import SearchInput, { createFilter } from 'react-search-input'
 
 type Props = {}
 
@@ -22,28 +20,19 @@ const BlogPage = (props: Props) => {
     }
     const { blogdata } = Blog()
 
-    const [search, setSearch] = useState('')
-    const searchUpdated = (term: React.SetStateAction<string>) => {
-        setSearch(term)
-    }
-    const KEYS_TO_FILTERS = ['name']
-
-    const filtered = blogdata?.filter(createFilter(search, KEYS_TO_FILTERS))
-
     return (
         <>
-            <SearchInput className="search-input" onChange={searchUpdated} />
             <div className="news_list">
                 <ul>
-                    {filtered?.map((blog) => (
-                        <Link href={`/blog-details/${blog?.id}`} key={blog?.id}>
+                    {blogdata?.map((blog) => (
+                        <Link href={`/blog-details/${blog.id}`} key={blog?.id}>
                             <li data-aos="fade-right" data-aos-duration="1200">
                                 <div className="list_inner">
                                     <div className="image">
                                         <div
                                             className="main"
                                             style={{
-                                                backgroundImage: `url(${blog?.image})`,
+                                                backgroundImage: `url(${blog.image})`,
                                             }}
                                         ></div>
                                     </div>
